25b18855a8e6dbaea1ea15294e46965a854157bd
Aisha Tammy <aisha@bsd.ac>
2024-08-28 17:56:57 -0400

    cleanup test

---
+++ regress/sys/sys/tree/rb-linux/rb-linux.d
@@ -0,0 +1,36 @@
+rb-linux.o: rb-linux.c /usr/include/sys/time.h /usr/include/sys/select.h \
+  /usr/include/sys/types.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/machine/endian.h \
+  /usr/include/time.h /usr/include/sys/_null.h /usr/include/sys/_time.h \
+  /usr/include/assert.h /usr/include/err.h /usr/include/stddef.h \
+  /usr/include/stdlib.h /usr/include/stdio.h /usr/include/string.h \
+  /usr/include/strings.h \
+  ../../../../../sys/dev/pci/drm/include/linux/container_of.h \
+  ../../../../../sys/dev/pci/drm/include/linux/rbtree.h \
+  /usr/include/sys/tree.h /usr/include/stdint.h
+/usr/include/sys/time.h:
+/usr/include/sys/select.h:
+/usr/include/sys/types.h:
+/usr/include/sys/cdefs.h:
+/usr/include/machine/cdefs.h:
+/usr/include/sys/endian.h:
+/usr/include/sys/_endian.h:
+/usr/include/sys/_types.h:
+/usr/include/machine/_types.h:
+/usr/include/machine/endian.h:
+/usr/include/time.h:
+/usr/include/sys/_null.h:
+/usr/include/sys/_time.h:
+/usr/include/assert.h:
+/usr/include/err.h:
+/usr/include/stddef.h:
+/usr/include/stdlib.h:
+/usr/include/stdio.h:
+/usr/include/string.h:
+/usr/include/strings.h:
+../../../../../sys/dev/pci/drm/include/linux/container_of.h:
+../../../../../sys/dev/pci/drm/include/linux/rbtree.h:
+/usr/include/sys/tree.h:
+/usr/include/stdint.h:
